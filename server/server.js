4436692daec2f3c522b6b0e1aa16b0bd28b48637
Arun Kumar <mymailarun32@gmail.com>
2024-06-08 00:12:25 +0530

    feat(server) : create boiler plate code for server using express js, nodemon, mongoose, mongodb, bcrypt, jsonwebtoken, dotenv, etc.

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+const userRouter = require("./routes/userRouter");
+const cors = require("cors");
+const restaurantRouter = require("./routes/restaurantRouter");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+console.log(dbConfig);
+
+app.use(express.json());
+app.use(cors());
+app.use("/app/v1/users", userRouter);
+
+app.use("/app/v1/users", restaurantRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
