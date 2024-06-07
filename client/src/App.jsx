5822d01bdc952d661037da0391ad4c60c7eb6766
Arun Kumar <mymailarun32@gmail.com>
2024-06-08 00:04:17 +0530

    feat(client) : implement boiler plate for client side using vite, tailwind CSS , material UI , etc.

---
+++ client/src/App.jsx
@@ -0,0 +1,56 @@
+import "./App.css";
+// import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/SIgnup/Signup";
+import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
+import Profile from "./pages/profile/Profile";
+import Admin from "./pages/admin/Admin";
+import { useSelector } from "react-redux";
+import { Spin } from "antd";
+
+function App() {
+  const { loading } = useSelector((state) => state.loader);
+  return (
+    <div>
+      {loading && (
+        <div className="flex justify-center items-center h-screen">
+          <Spin />
+        </div>
+      )}
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
