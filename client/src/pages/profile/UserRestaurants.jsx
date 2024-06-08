15abaada2b8e5dec8fed4188b154afd13fa48fd1
Arun Kumar <mymailarun32@gmail.com>
2024-06-08 13:38:52 +0530

    fix(userRestaurants): fix some ui bugs

--- client/src/pages/profile/UserRestaurants.jsx
+++ client/src/pages/profile/UserRestaurants.jsx
@@ -1,6 +1,18 @@
 import { useEffect, useState } from "react";
-import { getUserRestaurants } from "../../apiCalls/restaurant";
-import { message, Avatar, List, Card, Typography, Tag } from "antd";
+import {
+  getUserRestaurants,
+  //   updateRestaurant,
+} from "../../apiCalls/restaurant";
+import {
+  message,
+  Avatar,
+  List,
+  Card,
+  Typography,
+  Tag,
+  Button,
+  Space,
+} from "antd";
 import {
   EditOutlined,
   DeleteOutlined,
@@ -8,9 +20,13 @@ import {
   SyncOutlined,
 } from "@ant-design/icons";
 import { useSelector } from "react-redux";
+import RestaurantForm from "../restaurants/RestaurantForm";
 
 const UserRestaurants = () => {
   const [userRestaurants, setUserRestaurants] = useState([]);
+  const [toggleFormModal, setToggleFormModal] = useState(false);
+  const [userRestaurant, setUserRestaurant] = useState(null);
+  const [formType, setFormType] = useState("add");
   const { user } = useSelector((state) => state.users);
 
   const getData = async () => {
@@ -33,58 +49,98 @@ const UserRestaurants = () => {
 
   console.log(userRestaurants);
   return (
-    <List
-      itemLayout="vertical"
-      size="large"
-      dataSource={userRestaurants}
-      renderItem={(restaurant) => (
-        <Card size="small" className="mb-2">
-          <List.Item
-            key={restaurant._id}
-            actions={[
-              <EditOutlined key={restaurant._id} />,
-              <DeleteOutlined key={restaurant._id} />,
-              !restaurant.isActive ? (
-                <Tag
-                  icon={<SyncOutlined spin />}
-                  color="processing"
+    <>
+      <div className="w-full flex justify-end mb-4">
+        <Button onClick={() => setToggleFormModal(true)}>Add Restaurant</Button>
+      </div>
+      <List
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 1,
+          md: 1,
+          lg: 2,
+          xl: 2,
+          xxl: 2,
+        }}
+        dataSource={userRestaurants}
+        renderItem={(restaurant) => (
+          <List.Item key={restaurant._id}>
+            <Card
+              size="large"
+              actions={[
+                <EditOutlined
                   key={restaurant._id}
-                >
-                  Status : Pending
-                </Tag>
-              ) : (
-                <Tag
-                  icon={<CheckCircleOutlined />}
-                  color="success"
-                  key={restaurant._id}
-                >
-                  Status : Approved
-                </Tag>
-              ),
-            ]}
-          >
-            <List.Item.Meta
-              avatar={
-                <Avatar
-                  style={{
-                    backgroundColor: "#f56a00",
-                    verticalAlign: "middle",
+                  onClick={() => {
+                    setToggleFormModal(true);
+                    setFormType("edit");
+                    setUserRestaurant(restaurant);
                   }}
-                  size="large"
-                >
-                  {restaurant.name[0]}
-                </Avatar>
-              }
-              title={
-                <Typography.Title level={5}>{restaurant.name}</Typography.Title>
-              }
-              description={restaurant.address}
-            />
-            {restaurant?.description}
+                />,
+                <DeleteOutlined key={restaurant._id} />,
+                !restaurant.isActive ? (
+                  <Tag
+                    icon={<SyncOutlined spin />}
+                    color="processing"
+                    key={restaurant._id}
+                  >
+                    Pending
+                  </Tag>
+                ) : (
+                  <Tag
+                    icon={<CheckCircleOutlined />}
+                    color="success"
+                    key={restaurant._id}
+                  >
+                    Active
+                  </Tag>
+                ),
+              ]}
+            >
+              <Card.Meta
+                avatar={
+                  <Avatar
+                    style={{
+                      backgroundColor: "#f56a00",
+                      verticalAlign: "middle",
+                    }}
+                    size="large"
+                  >
+                    {restaurant.name[0]}
+                  </Avatar>
+                }
+                title={
+                  <Space direction="vertical" size={0}>
+                    <Typography.Title level={5} style={{ margin: 0 }}>
+                      {restaurant.name}
+                    </Typography.Title>
+                    <Typography.Text
+                      type="secondary"
+                      className="text-sm font-normal"
+                    >
+                      {restaurant.address}
+                    </Typography.Text>
+                  </Space>
+                }
+                description={
+                  <Typography.Text>{restaurant.description}</Typography.Text>
+                }
+              />
+            </Card>
           </List.Item>
-        </Card>
+        )}
+      />
+      {toggleFormModal && (
+        <RestaurantForm
+          toggleFormModal={toggleFormModal}
+          setToggleFormModal={setToggleFormModal}
+          userRestaurant={userRestaurant}
+          formType={formType}
+          setUserRestaurant={setUserRestaurant}
+          getData={getData}
+        />
       )}
-    />
+    </>
   );
 };
 
