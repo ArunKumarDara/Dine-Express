a30c519aca7ed30adae7f7ae007c6706f5cb4b2d
Arun Kumar <mymailarun32@gmail.com>
2024-06-09 10:13:59 +0530

    fix(restaurantRouter): add validateJwt middleware

--- server/routes/restaurantRouter.js
+++ server/routes/restaurantRouter.js
@@ -6,13 +6,18 @@ const {
   deleteRestaurant,
   getRestaurantsByOwnerId,
 } = require("../controller/restaurantController");
+const validateJwtToken = require("../middleware/authMiddleware");
 
 const restaurantRouter = express.Router();
 
-restaurantRouter.get("/getAllRestaurants", getAllRestaurants);
-restaurantRouter.post("/addRestaurant", addRestaurant);
-restaurantRouter.post("/editRestaurant", updateRestaurant);
-restaurantRouter.post("/getRestaurantsByOwner", getRestaurantsByOwnerId);
-restaurantRouter.post("/deleteRestaurant", deleteRestaurant);
+restaurantRouter.get("/getAllRestaurants", validateJwtToken, getAllRestaurants);
+restaurantRouter.post("/addRestaurant", validateJwtToken, addRestaurant);
+restaurantRouter.post("/editRestaurant", validateJwtToken, updateRestaurant);
+restaurantRouter.post(
+  "/getRestaurantsByOwner",
+  validateJwtToken,
+  getRestaurantsByOwnerId
+);
+restaurantRouter.post("/deleteRestaurant", validateJwtToken, deleteRestaurant);
 
 module.exports = restaurantRouter;
