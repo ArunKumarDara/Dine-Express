025db9cb5874998541a176a9f8fbb0220ee4777d
Arun Kumar <mymailarun32@gmail.com>
2024-06-10 09:10:39 +0530

    fix{protectedRoute): remove search bar.

--- client/src/components/protectedRoute/ProtectedRoute.jsx
+++ client/src/components/protectedRoute/ProtectedRoute.jsx
@@ -1,14 +1,11 @@
 import { useState, useEffect } from "react";
-import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MoreIcon from "@mui/icons-material/MoreVert";
 import { getCurrentUser } from "../../apiCalls/user";
@@ -17,45 +14,6 @@ import { useSelector, useDispatch } from "react-redux";
 import { setUser } from "../../redux/userSlice";
 import { useNavigate } from "react-router-dom";
 import { showLoading, hideLoading } from "../../redux/loaderSlice";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
 
 // eslint-disable-next-line react/prop-types
 export default function ProtectedRoute({ children }) {
@@ -158,18 +116,6 @@ export default function ProtectedRoute({ children }) {
       onClose={handleMobileMenuClose}
     >
       <MenuItem>
-        {/* <Typography
-          fontSize="16px"
-          noWrap
-          component="div"
-          sx={{
-            cursor: "pointer",
-          }}
-        >
-          Add Restaurant
-        </Typography> */}
-      </MenuItem>
-      <MenuItem>
         <Typography
           fontSize="16px"
           noWrap
@@ -194,34 +140,12 @@ export default function ProtectedRoute({ children }) {
                 variant="h6"
                 noWrap
                 component="div"
-                sx={{ display: { xs: "none", sm: "block" }, cursor: "pointer" }}
+                sx={{ cursor: "pointer" }}
                 onClick={() => navigate("/")}
               >
                 DineExpress
               </Typography>
-              <Search>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  placeholder="Search Restaurants"
-                  inputProps={{ "aria-label": "search" }}
-                />
-              </Search>
               <Box sx={{ flexGrow: 1 }} />
-              {/* <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{
-                  display: { xs: "none", sm: "block" },
-                  mr: 4,
-                  cursor: "pointer",
-                }}
-                onClick={() => setToggleFormModal(true)}
-              >
-                Add Restaurant
-              </Typography> */}
               <Typography
                 variant="h6"
                 noWrap
