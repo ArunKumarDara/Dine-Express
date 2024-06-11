cc54d235d8babba02a6eabfe8c4177cba3fd1b33
Arun Kumar <mymailarun32@gmail.com>
2024-06-11 12:24:34 +0530

    feat(orderModel): create order model

---
+++ server/model/orderModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const orderSchema = new Schema(
+  {
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    restaurant: {
+      type: Schema.Types.ObjectId,
+      ref: "restaurants",
+      required: true,
+    },
+    orderItems: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "items",
+      },
+    ],
+    status: {
+      type: String,
+      enum: [
+        "Pending",
+        "Confirmed",
+        "Preparing",
+        "Ready",
+        "Completed",
+        "Cancelled",
+      ],
+      default: "Pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("orders", orderSchema);
