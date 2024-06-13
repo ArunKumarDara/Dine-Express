656b98f5151a90db35c10c2726ef84fc3c032980
Arun Kumar <mymailarun32@gmail.com>
2024-06-13 20:25:42 +0530

    feat(myprofile): show user orders, restaurants, and account in tabs mode

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,5 +1,32 @@
+import { Tabs } from "antd";
+import UserRestaurants from "./UserRestaurants";
+import UserOrders from "./UserOrders";
+import MyAccount from "./MyAccount";
+
+const items = [
+  {
+    key: "1",
+    label: "My Restaurants",
+    children: <UserRestaurants />,
+  },
+  {
+    key: "2",
+    label: "Orders",
+    children: <UserOrders />,
+  },
+  {
+    key: "3",
+    label: "Account",
+    children: <MyAccount />,
+  },
+];
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="m-4">
+      <Tabs items={items} defaultActiveKey="1" type="card" className="mt-2" />
+    </div>
+  );
 };
 
 export default Profile;
