acbef7ad2dad6fbee985cf485e6724cb89b2d6eb
Arun Kumar <mymailarun32@gmail.com>
2024-06-13 20:19:28 +0530

    feat(components/Spinner): create component for spinner

---
+++ client/src/components/spinner/Spinner.jsx
@@ -0,0 +1,11 @@
+import { Spin } from "antd";
+
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center w-full">
+      <Spin />
+    </div>
+  );
+};
+
+export default Spinner;
