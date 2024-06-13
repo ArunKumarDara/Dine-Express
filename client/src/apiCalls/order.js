d520bdaa78e5e2a2df3e9053588a7d2810fa1c92
Arun Kumar <mymailarun32@gmail.com>
2024-06-13 20:21:38 +0530

    fix(apiCalls/order): change functionality for add orders and order items

--- client/src/apiCalls/order.js
+++ client/src/apiCalls/order.js
@@ -1,9 +1,28 @@
 import { axiosInstance } from ".";
 
-export const addOrder = async (payload) => {
+export const addOrder = async ({
+  totalAmount,
+  user,
+  restaurant,
+  orderItems,
+}) => {
+  const payload1 = { totalAmount, user, restaurant };
+  const payload2 = orderItems;
+  try {
+    const response = await axiosInstance.post("/app/v1/users/addOrder", {
+      payload1,
+      payload2,
+    });
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getOrdersByUserId = async (payload) => {
   try {
     const response = await axiosInstance.post(
-      "/app/v1/users/addOrder",
+      "/app/v1/users/getOrdersByUserId",
       payload
     );
     return response.data;
