de3372ce1a3d2d08b72778b4d0b199f0d908b467
Arun Kumar <mymailarun32@gmail.com>
2024-06-15 08:56:24 +0530

    feat(addressRouter): create router for getting all the addresses for the selected user

--- server/routes/addressRouter.js
+++ server/routes/addressRouter.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { addAddress } = require("../controller/addressController");
+const {
+  addAddress,
+  getAllAddressByUser,
+} = require("../controller/addressController");
 const validateJwtToken = require("../middleware/authMiddleware");
 
 const addressRouter = express.Router();
 
 addressRouter.post("/addAddress", validateJwtToken, addAddress);
+addressRouter.post(
+  "/getAllAddressByUser",
+  validateJwtToken,
+  getAllAddressByUser
+);
 
 module.exports = addressRouter;
