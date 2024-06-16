c3d1456394f38f4704a4735506a27a6c1042948c
Arun Kumar <mymailarun32@gmail.com>
2024-06-16 10:10:32 +0530

    feat(addressController): functionality for select primary address

--- server/controller/addressController.js
+++ server/controller/addressController.js
@@ -2,7 +2,13 @@ const addressModel = require("../model/addressModel");
 
 const addAddress = async (req, res) => {
   try {
-    console.log(req.body);
+    const userAddresses = await addressModel.find({ user: req.body.userId });
+    if (req.body.isPrimary) {
+      userAddresses.forEach(async (userAddress) => {
+        userAddress.isPrimary = false;
+        await userAddress.save();
+      });
+    }
     const address = new addressModel({ ...req.body, user: req.body.userId });
     const response = await address.save();
     res.status(200).json({
@@ -36,6 +42,14 @@ const getAllAddressByUser = async (req, res) => {
 
 const editAddress = async (req, res) => {
   try {
+    console.log(req.body);
+    const userAddresses = await addressModel.find({ user: req.body.userId });
+    if (req.body.address.isPrimary) {
+      userAddresses.forEach(async (userAddress) => {
+        userAddress.isPrimary = false;
+        await userAddress.save();
+      });
+    }
     const response = await addressModel.findByIdAndUpdate(
       req.body.addressId,
       req.body.address
@@ -68,9 +82,29 @@ const deleteAddress = async (req, res) => {
   }
 };
 
+const getPrimaryAddress = async (req, res) => {
+  try {
+    const response = await addressModel.findOne({
+      user: req.body.userId,
+      isPrimary: true,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Primary address fetched successfully",
+      data: response,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message || "Cannot fetch primary address",
+    });
+  }
+};
+
 module.exports = {
   addAddress,
   getAllAddressByUser,
   editAddress,
   deleteAddress,
+  getPrimaryAddress,
 };
