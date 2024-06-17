135a738da3fa0f464ccf7ea374fa94a10e2ea363
Arun Kumar <mymailarun32@gmail.com>
2024-06-18 01:32:06 +0530

    feat(admin): show restaurants, orders and account tabes for admin

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -1,5 +1,32 @@
+import { Tabs } from "antd";
+import Restaurants from "./Restaurants";
+import Orders from "./Orders";
+import Account from "./Account";
+
+const items = [
+  {
+    key: "1",
+    label: "Restaurants",
+    children: <Restaurants />,
+  },
+  {
+    key: "2",
+    label: "Orders",
+    children: <Orders />,
+  },
+  {
+    key: "3",
+    label: "Account",
+    children: <Account />,
+  },
+];
+
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <div className="m-4">
+      <Tabs items={items} defaultActiveKey="2" type="card" className="mt-2" />
+    </div>
+  );
 };
 
 export default Admin;
