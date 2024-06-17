72b7f33301a01e4d33143b1b75dc4e8479667b4a
Arun Kumar <mymailarun32@gmail.com>
2024-06-18 01:26:03 +0530

    fix(restaurantModel): add timestamps

--- server/model/restaurantModel.js
+++ server/model/restaurantModel.js
@@ -1,35 +1,40 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const restaurantSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const restaurantSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
   },
-  address: {
-    type: String,
-    required: true,
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: "users",
-    required: true,
-  },
-  rating: {
-    type: Number,
-    default: 0,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  isActive: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("restaurants", restaurantSchema);
