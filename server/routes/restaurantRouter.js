4521daee390751a76e5e096ec4627316512ff701
Arun Kumar <mymailarun32@gmail.com>
2024-06-18 01:29:11 +0530

    feat(restaurantRouter): create router for get all restaurants and actions on restaurants

--- server/routes/restaurantRouter.js
+++ server/routes/restaurantRouter.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const {
-  getAllRestaurants,
+  getAllActiveRestaurants,
   addRestaurant,
   updateRestaurant,
   deleteRestaurant,
   getRestaurantsByOwnerId,
+  getAllRestaurants,
+  actionOnRestaurant,
 } = require("../controller/restaurantController");
 const validateJwtToken = require("../middleware/authMiddleware");
 
 const restaurantRouter = express.Router();
 
+restaurantRouter.get(
+  "/getAllActiveRestaurants",
+  validateJwtToken,
+  getAllActiveRestaurants
+);
 restaurantRouter.get("/getAllRestaurants", validateJwtToken, getAllRestaurants);
+restaurantRouter.post(
+  "/actionOnRestaurant",
+  validateJwtToken,
+  actionOnRestaurant
+);
 restaurantRouter.post("/addRestaurant", validateJwtToken, addRestaurant);
 restaurantRouter.post("/editRestaurant", validateJwtToken, updateRestaurant);
 restaurantRouter.post(
