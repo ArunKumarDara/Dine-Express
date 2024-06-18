5f14627ef6784b39e5d0a506f180d5226229452c
Arun Kumar <mymailarun32@gmail.com>
2024-06-18 16:22:20 +0530

    feat(orderRouter): create router for orders controlled by admin

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -3,10 +3,12 @@ const orderRouter = express.Router();
 const {
   addOrder,
   getOrdersByUserId,
+  getAllOrders,
 } = require("../controller/orderController");
 const validateJwtToken = require("../middleware/authMiddleware");
 
 orderRouter.post("/addOrder", validateJwtToken, addOrder);
 orderRouter.post("/getOrdersByUserId", validateJwtToken, getOrdersByUserId);
+orderRouter.get("/getAllOrders", validateJwtToken, getAllOrders);
 
 module.exports = orderRouter;
