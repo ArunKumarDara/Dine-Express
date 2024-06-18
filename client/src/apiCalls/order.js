fcb922b97a741c587179bbe96e75100d691a86df
Arun Kumar <mymailarun32@gmail.com>
2024-06-18 16:27:09 +0530

    feat(apiCall/order): api call for send delivery address to server

--- client/src/apiCalls/order.js
+++ client/src/apiCalls/order.js
@@ -4,9 +4,10 @@ export const addOrder = async ({
   totalAmount,
   user,
   restaurant,
+  deliverTo,
   orderItems,
 }) => {
-  const payload1 = { totalAmount, user, restaurant };
+  const payload1 = { totalAmount, user, restaurant, deliverTo };
   const payload2 = orderItems;
   try {
     const response = await axiosInstance.post("/app/v1/users/addOrder", {
@@ -25,6 +26,15 @@ export const getOrdersByUserId = async (payload) => {
       "/app/v1/users/getOrdersByUserId",
       payload
     );
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getAllOrders = async () => {
+  try {
+    const response = await axiosInstance.get("/app/v1/admin/getAllOrders");
     return response.data;
   } catch (error) {
     return error;
