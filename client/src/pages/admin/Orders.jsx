e758e6c6b19b9d63e4ff6a85a56b12afdd3f976d
Arun Kumar <mymailarun32@gmail.com>
2024-06-19 16:04:36 +0530

    feat(orders): implement functionality for update order status

--- client/src/pages/admin/Orders.jsx
+++ client/src/pages/admin/Orders.jsx
@@ -1,107 +1,25 @@
-import { Table, message, Tag, Typography, Space } from "antd";
+import { Table, message, Tag, Typography, Space, Button, Modal } from "antd";
 import { useEffect, useState } from "react";
-import { getAllOrders } from "../../apiCalls/order";
+import { getAllOrders, updateOrderStatus } from "../../apiCalls/order";
 import moment from "moment";
 import {
   CloseCircleOutlined,
   ClockCircleOutlined,
   CheckCircleOutlined,
+  ExclamationCircleOutlined,
 } from "@ant-design/icons";
 import Spinner from "../../components/spinner/Spinner";
 
-const columns = [
-  {
-    title: "Restaurant",
-    dataIndex: "name",
-    render: (text, record) => {
-      return record.restaurant.name;
-    },
-  },
-  {
-    title: "Ordered by",
-    dataIndex: "name",
-    render: (text, record) => {
-      return (
-        <Space direction="vertical">
-          <Typography.Text>{`${record.user.firstName} ${record.user.lastName},`}</Typography.Text>
-          <Typography.Text>{`+91-${record.user.phoneNumber},`}</Typography.Text>
-          <Typography.Text>{record.user.email}</Typography.Text>
-        </Space>
-      );
-    },
-  },
-  {
-    title: "Delivery Address",
-    dataIndex: "deliverTo",
-    render: (text) => {
-      return (
-        <Space direction="vertical">
-          <Typography.Text>{`${text?.addressLine1},`}</Typography.Text>
-          <Typography.Text>{`${text.addressLine2},`}</Typography.Text>
-          <Typography.Text>{`${text.state}`}</Typography.Text>
-          <Typography.Text>{`${text.city}, ${text.landmark}`}</Typography.Text>
-          <Typography.Text>{text.pinCode}</Typography.Text>
-        </Space>
-      );
-    },
-  },
-  {
-    title: "Ordered on",
-    dataIndex: "createdAt",
-    render: (text) => {
-      return moment(text).format("MMMM Do, YYYY");
-    },
-  },
-  {
-    title: "Ordered Items",
-    render: (text, record) => {
-      return record.menuItems.map((menuItem) => (
-        <div key={menuItem.item._id}>
-          <Typography.Text>
-            {menuItem.item.name}
-            <span>- </span>
-          </Typography.Text>
-          <Typography.Text>
-            {menuItem.quantity}
-            <span>,</span>
-          </Typography.Text>
-        </div>
-      ));
-    },
-  },
-  {
-    title: "Amount",
-    dataIndex: "totalAmount",
-    render: (text) => {
-      return `₹${text}`;
-    },
-  },
-  {
-    title: "Status",
-    dataIndex: "status",
-    render: (text) => {
-      if (text === "Pending" || text === "Confirmed" || text === "Preparing") {
-        return (
-          <Tag icon={<ClockCircleOutlined />} color="processing">
-            {text}
-          </Tag>
-        );
-      } else if (text === "Cancelled") {
-        return (
-          <Tag icon={<CloseCircleOutlined />} color="error">
-            {text}
-          </Tag>
-        );
-      } else if (text === "Completed" || text === "Ready") {
-        return (
-          <Tag icon={<CheckCircleOutlined />} color="success">
-            {text}
-          </Tag>
-        );
-      }
-    },
-  },
-];
+const { confirm } = Modal;
+
+const nextStatus = {
+  Pending: ["Confirm", "Cancel"],
+  Confirm: ["Preparing"],
+  Preparing: ["Ready"],
+  Ready: ["On the way"],
+  "On the way": ["Completed"],
+  Completed: ["Deliver"],
+};
 
 const Orders = () => {
   const [orders, setOrders] = useState(null);
@@ -124,6 +42,151 @@ const Orders = () => {
     }
   };
 
+  const handleStatusChange = (orderId, newStatus) => {
+    confirm({
+      title: `Do you want to update the order status to ${newStatus}?`,
+      icon: <ExclamationCircleOutlined />,
+      onOk: async () => {
+        try {
+          const response = await updateOrderStatus(orderId, newStatus);
+          if (response.success) {
+            message.success(`Order status updated to ${newStatus}`);
+            getData();
+          } else {
+            message.error(response.message);
+          }
+        } catch (error) {
+          message.error("Failed to update order status. Please try again.");
+        }
+      },
+    });
+  };
+
+  const columns = [
+    {
+      title: "Restaurant",
+      dataIndex: "name",
+      render: (text, record) => {
+        return record.restaurant.name;
+      },
+    },
+    {
+      title: "Ordered by",
+      dataIndex: "name",
+      render: (text, record) => {
+        return (
+          <Space direction="vertical">
+            <Typography.Text>{`${record.user.firstName} ${record.user.lastName},`}</Typography.Text>
+            <Typography.Text>{`+91-${record.user.phoneNumber},`}</Typography.Text>
+            <Typography.Text>{record.user.email}</Typography.Text>
+          </Space>
+        );
+      },
+    },
+    {
+      title: "Delivery Address",
+      dataIndex: "deliverTo",
+      render: (text) => {
+        return (
+          <Space direction="vertical">
+            <Typography.Text>{`${text?.addressLine1},`}</Typography.Text>
+            <Typography.Text>{`${text.addressLine2},`}</Typography.Text>
+            <Typography.Text>{`${text.state}`}</Typography.Text>
+            <Typography.Text>{`${text.city}, ${text.landmark}`}</Typography.Text>
+            <Typography.Text>{text.pinCode}</Typography.Text>
+          </Space>
+        );
+      },
+    },
+    {
+      title: "Ordered on",
+      dataIndex: "createdAt",
+      render: (text) => {
+        return moment(text).format("MMMM Do, YYYY");
+      },
+    },
+    {
+      title: "Ordered Items",
+      render: (text, record) => {
+        return record.menuItems.map((menuItem) => (
+          <div key={menuItem.item._id}>
+            <Typography.Text>
+              {menuItem.item.name}
+              <span>- </span>
+            </Typography.Text>
+            <Typography.Text>
+              {menuItem.quantity}
+              <span>,</span>
+            </Typography.Text>
+          </div>
+        ));
+      },
+    },
+    {
+      title: "Amount",
+      dataIndex: "totalAmount",
+      render: (text) => {
+        return `₹${text}`;
+      },
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      render: (text) => {
+        if (text === "Pending" || text === "Confirm" || text === "Preparing") {
+          return (
+            <Tag icon={<ClockCircleOutlined />} color="processing">
+              {text === "Confirm" ? "Confirmed" : text}
+            </Tag>
+          );
+        } else if (text === "Cancelled") {
+          return (
+            <Tag icon={<CloseCircleOutlined />} color="error">
+              {text}
+            </Tag>
+          );
+        } else if (
+          text === "Completed" ||
+          text === "Ready" ||
+          text === "Deliver"
+        ) {
+          return (
+            <Tag icon={<CheckCircleOutlined />} color="success">
+              {text === "Deliver" ? "Delivered" : text}
+            </Tag>
+          );
+        }
+      },
+    },
+    {
+      title: "Action",
+      key: "action",
+      fixed: "right",
+      width: 100,
+      render: (text, record) => {
+        const availableActions = nextStatus[record.status] || [];
+        return (
+          <Space direction="vertical">
+            {availableActions.map((status) => (
+              <Button
+                type="link"
+                key={status}
+                onClick={() =>
+                  handleStatusChange(
+                    record._id,
+                    status === "Cancel" ? "Cancelled" : status
+                  )
+                }
+              >
+                {status}
+              </Button>
+            ))}
+          </Space>
+        );
+      },
+    },
+  ];
+
   const handleTableChange = (pagination) => {
     setPagination(pagination);
   };
@@ -131,8 +194,6 @@ const Orders = () => {
   useEffect(() => {
     getData();
   }, []);
-
-  console.log(orders);
 
   return (
     <>
@@ -150,7 +211,7 @@ const Orders = () => {
           }}
           onChange={handleTableChange}
           scroll={{
-            x: 800,
+            x: 1200,
           }}
         />
       )}
