4188af96182fadf2384cb0b7965b45ba1370a804
Arun Kumar <mymailarun32@gmail.com>
2024-07-05 14:01:32 +0530

    feat(userRouter): add router for receiver details crud operations

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,6 +3,8 @@ const {
   registerUser,
   loginUser,
   getCurrentUser,
+  addReceiverDetails,
+  getReceiverDetails,
 } = require("../controller/userController");
 const validateJwtToken = require("../middleware/authMiddleware");
 const userRouter = express.Router();
@@ -10,5 +12,7 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/getCurrentUser", validateJwtToken, getCurrentUser);
+userRouter.post("/addReceiverDetails", validateJwtToken, addReceiverDetails);
+userRouter.post("/getReceiverDetails", validateJwtToken, getReceiverDetails);
 
 module.exports = userRouter;
