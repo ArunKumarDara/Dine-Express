011e48e07e80af3d6a5216b00e62d988172fc312
Arun Kumar <mymailarun32@gmail.com>
2024-07-05 15:22:55 +0530

    fix base url bug

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "https://dine-express-backend.onrender.com",
+  // baseURL: "http://localhost:8080",
   timeout: 10000,
   headers: {
     credentials: "include",
