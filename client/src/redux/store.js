0eccfa3346332c8efac59cb9d53fdc6543e8b4f9
Arun Kumar <mymailarun32@gmail.com>
2024-07-10 16:05:35 +0530

    feat(store): configure store for cartSlice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./userSlice";
 import loaderReducer from "./loaderSlice";
+import cartReducer from "./cartSlice";
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
     loader: loaderReducer,
+    cart: cartReducer,
   },
 });
 
