718a372dc782ba59984a89f2ca50793c5e5339f9
Arun Kumar <mymailarun32@gmail.com>
2024-07-14 20:23:47 +0530

    fix(apiCalls/payment): fix response

--- client/src/apiCalls/payment.js
+++ client/src/apiCalls/payment.js
@@ -2,11 +2,10 @@ import { axiosInstance } from ".";
 
 export const makePayment = async ({ cart, orderSummary }) => {
   try {
-    const response = await axiosInstance.post("/app/v1/users/makePayment", {
+    await axiosInstance.post("/app/v1/users/makePayment", {
       cart,
       orderSummary,
     });
-    return response.data;
   } catch (error) {
     return error;
   }
