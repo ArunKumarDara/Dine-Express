ac0ed3256fd022eae8cfb7351a970bbc5a59d96c
Arun Kumar <mymailarun32@gmail.com>
2024-07-15 15:02:30 +0530

    fix(server): change cors origin url

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://dine-express-server.vercel.app",
+    origin: "https://dine-express.vercel.app",
   })
 );
 
