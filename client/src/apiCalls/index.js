36f07398ded6b5baf0e651981e516473d2d1ea3b
Arun Kumar <mymailarun32@gmail.com>
2024-07-15 22:45:24 +0530

    fix(apiCall/index): change base URL

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "http://localhost:8080",
-  baseURL: "https://dine-express-server.vercel.app",
+  baseURL: "http://localhost:8080",
   timeout: 10000,
   headers: {
     credentials: "include",
