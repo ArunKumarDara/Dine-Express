8cf90858a225f8a93fad18a271e2eb5254f3f59f
Arun Kumar <mymailarun32@gmail.com>
2024-07-15 22:44:31 +0530

    feat(server): create functionality for sending static file for production level

--- server/server.js
+++ server/server.js
@@ -9,25 +9,13 @@ const orderRouter = require("./routes/orderRouter");
 const addressRouter = require("./routes/addressRouter");
 const dashBoardRouter = require("./routes/dashBoardRouter");
 const paymentRouter = require("./routes/makePaymentRouter");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://dine-express.vercel.app/",
-    credentials: true,
-  })
-);
-
-app.options(
-  "*",
-  cors({
-    origin: "https://dine-express.vercel.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/app/v1/users", userRouter);
 app.use("/app/v1/users", restaurantRouter);
@@ -39,9 +27,17 @@ app.use("/app/v1/admin", restaurantRouter);
 app.use("/app/v1/admin", orderRouter);
 app.use("/app/v1/admin", dashBoardRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "client", "dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
