177d10ff8ddf03ff9455e2637c3b35f3974429a0
Arun Kumar <mymailarun32@gmail.com>
2024-07-16 15:48:54 +0530

    fix(home): change ui view

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -121,16 +121,16 @@ const Home = () => {
                 <Skeleton active />
               </Col>
             ) : (
-              <Col xs={24} lg={12}>
+              <Col xs={24} lg={16}>
                 <List
                   grid={{
                     gutter: 16,
                     xs: 1,
                     sm: 1,
                     md: 1,
-                    lg: 1,
-                    xl: 1,
-                    xxl: 1,
+                    lg: 2,
+                    xl: 2,
+                    xxl: 3,
                   }}
                   dataSource={filteredRestaurants}
                   renderItem={(restaurant) => (
