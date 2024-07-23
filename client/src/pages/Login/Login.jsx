3b35705ea65fdc72319d7a908fc18ae63207c666
Arun Kumar <mymailarun32@gmail.com>
2024-07-23 15:16:27 +0530

    feat(login): redesign login page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,11 +1,26 @@
-import { Form, Input, Typography, Space, message, Row, Col, Card } from "antd";
-import { loginUser } from "../../apiCalls/user";
+import Avatar from "@mui/material/Avatar";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { Divider, Typography, message } from "antd";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Oauth from "../../components/oauth/Oauth";
+import { loginUser } from "../../apiCalls/user";
 
-const Login = () => {
-  const handleFinish = async (values) => {
+const defaultTheme = createTheme();
+
+export default function Login() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     try {
-      const response = await loginUser(values);
+      const data = new FormData(event.currentTarget);
+      const response = await loginUser({
+        email: data.get("email"),
+        password: data.get("password"),
+      });
       if (response.success) {
         localStorage.setItem("tokenForDineExpress", response.data);
         window.location.href = "/";
@@ -13,68 +28,70 @@ const Login = () => {
         message.error(response.message);
       }
     } catch (error) {
-      message(error);
+      message.error(error);
     }
   };
 
   return (
-    <Row className="justify-center items-center h-screen">
-      <Col xs={20} md={12} lg={8}>
-        <Card className="shadow-lg">
-          <Typography.Title level={5} className="text-center">
-            Welcome to Dine Express, Please Login!
-          </Typography.Title>
-          <hr />
-          <Form layout="vertical" onFinish={handleFinish} className="mt-4">
-            <Form.Item
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography.Title level={3}>Sign in</Typography.Title>
+          <Oauth />
+          <Divider
+            className="uppercase text-gray-500"
+            plain
+            style={{ border: "gray" }}
+          >
+            or
+          </Divider>
+          <Box component="form" onSubmit={handleSubmit} noValidate>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
               label="Email"
               name="email"
-              rules={[
-                {
-                  type: "email",
-                  required: true,
-                  message: "Please enter your email!",
-                },
-              ]}
-            >
-              <Input size="large" type="email" placeholder="Enter your email" />
-            </Form.Item>
-            <Form.Item
-              label="Password"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
               name="password"
-              rules={[
-                {
-                  type: "password",
-                  required: true,
-                  message: "Please enter your password!",
-                },
-              ]}
-            >
-              <Input
-                size="large"
-                type="password"
-                placeholder="Enter your password"
-              />
-            </Form.Item>
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
             <button
               type="submit"
-              className="w-full font-semibold text-white bg-orange-500 h-10"
+              className="w-full font-semibold text-white bg-orange-500 rounded-md mt-4 mb-3 p-3"
             >
               LOGIN
             </button>
-          </Form>
-          <Space className="mt-2">
-            <Typography.Text>New To DineExpress? </Typography.Text>
-            <Link to="/signup">
-              <Typography.Text type="danger" className="cursor-pointer">
-                Create account
+            <Link to="/signup" variant="body2">
+              <Typography.Text className="text-center ">
+                Don&apos;t have an account?{" "}
+                <span className="hover:text-blue-500">Sign Up</span>
               </Typography.Text>
             </Link>
-          </Space>
-        </Card>
-      </Col>
-    </Row>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
-};
-
-export default Login;
+}
