4d05f314db3c0828d5b5c7a2795d644f2f87bf91
Arun Kumar <mymailarun32@gmail.com>
2025-02-05 23:46:44 +0530

    fix(server/authMiddleware): optimize authentication middleware function

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,14 +2,34 @@ const jwt = require("jsonwebtoken");
 
 const validateJwtToken = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.cookies.authToken;
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Authorization token is missing",
+      });
+    }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.body.userId = decoded.userId;
     next();
   } catch (error) {
-    res.status(400).json({
+    console.error("JWT verification error:", error);
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        success: false,
+        message: "Token has expired",
+      });
+    }
+
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token",
+      });
+    }
+    res.status(401).json({
       success: false,
-      message: "Invalid Token",
+      message: "Authentication failed",
     });
   }
 };
