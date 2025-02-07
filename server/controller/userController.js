a8bae5d1cb291ac625387c8198278a440df9d5d0
Arun Kumar <mymailarun32@gmail.com>
2025-02-07 16:11:10 +0530

    fix bugs

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -56,7 +56,7 @@ const loginUser = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: "strict",
+      sameSite: "lax",
     });
     return res.status(200).json({
       success: true,
