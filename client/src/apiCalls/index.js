a8bac6362dcae53ea7b94085e659acc2129cc331
Arun Kumar <mymailarun32@gmail.com>
2025-02-07 15:30:02 +0530

    feat(client): change base url to local server

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://dine-express.onrender.com",
-  // baseURL: "http://localhost:8080",
-  timeout: 10000,
+  baseURL: "http://localhost:8080",
+  withCredentials: true,
   headers: {
-    credentials: "include",
     "Content-Type": "application/json",
-    Authorization: `bearer ${
-      localStorage?.getItem("tokenForDineExpress") || ""
-    }`,
   },
 });
