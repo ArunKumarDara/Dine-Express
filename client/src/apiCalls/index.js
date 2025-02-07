f2243118f7b55b3a2735b6f3433042464cdb488c
Arun Kumar <mymailarun32@gmail.com>
2025-02-07 16:10:56 +0530

    fix erros

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://dine-express.onrender.com" || "http://localhost:8080",
+  baseURL: "https://dine-express.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
