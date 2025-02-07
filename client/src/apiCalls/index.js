427cbc4eb96dfb34e3f7d1b54f6cf1251fd49abe
Arun Kumar <mymailarun32@gmail.com>
2025-02-07 15:36:50 +0530

    feat:add backend url

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://dine-express.onrender.com" || "http://localhost:8080",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
